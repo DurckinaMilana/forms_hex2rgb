6c97ea52dd9227b24413648b686dd56492ab0bf9
Milana <durckina.milana2016@yandex.ru>
2023-08-11 22:44:22 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ra-forms-hex2rgb/'
+  base: '/forms_hex2rgb/'
 })
